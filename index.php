b004235fb024bfebc92bb5e961c28e8d20a3dc59
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2023-11-18 13:50:00 +0100

    add animations (final commit)

--- index.php
+++ index.php
@@ -74,51 +74,53 @@
             if ($result && mysqli_num_rows($result) > 0) {
                 while ($row = mysqli_fetch_assoc($result)) {
             ?>
-            <div class="flex flex-col justify-between items-center w-96 bg-white p-4 shadow-md rounded-md"
-                style="height: 550px;margin-left: 50px;">
+            <div class="flex flex-col justify-between w-96 bg-white p-4 shadow-md rounded-md"
+                style="height:550px;margin-left: 50px;">
 
                 <img src="<?php echo str_replace('../', '', $row['Image']); ?>" alt="Announcement Image"
-                    class="w-full h-80 bg-cover  object-cover mb-2">
+                    class="w-full h-96 bg-cover  object-cover ">
 
-                <h2 class="text-lg font-black">
-                    <?php echo $row['Title']; ?>
-
-                </h2>
-                <p class="my-2 text-red-500 font-mono font-bold">
-                    <?php echo $row['Price'];echo" "; echo" MAD"; ?>
-                </p>
-                <p class="text-gray-600 font-bold">
-                    <?php echo $row['Category']; ?>
-                </p>
-                <p class="text-gray-600 font-light">
-                    <?php echo $row['Description']; ?>
-                </p>
-                <div class="flex flex-row justify-between" style="margin-left: -120px;">
-
-                    <a class="mx-32 my-10"
-                        href="http://localhost/IlyasChaoui-Avito/UpdateTask.php?id=<?php echo $row['Id']; ?>"><svg
-                            xmlns="http://www.w3.org/2000/svg" height="30"
-                            viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                            <style>
-                                svg {
-                                    fill: #000000
-                                }
-                            </style>
-                            <path
-                                d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z" />
-                        </svg></a>
-                    <a class="my-10"
-                        href="http://localhost/IlyasChaoui-Avito/includes/delete.php?id=<?php echo $row['Id']; ?>"><svg
-                            xmlns="http://www.w3.org/2000/svg" height="30"
-                            viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                            <style>
-                                svg {
-                                    fill: #000000
-                                }
-                            </style>
-                            <path
-                                d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
-                        </svg></a>
+                <div class="">
+                    <h2 class=" text-lg font-black">
+                        <?php echo $row['Title']; ?>
+    
+                    </h2>
+                    <p class="my-2 text-red-500 font-mono font-bold">
+                        <?php echo $row['Price'];echo" "; echo" MAD"; ?>
+                    </p>
+                    <p class=" my-2text-gray-600 font-bold">
+                        <?php echo $row['Category']; ?>
+                    </p>
+                    <p class=" my-2text-gray-600 font-light">
+                        <?php echo $row['Description']; ?>
+                    </p>
+                    <div class="flex flex-row justify-between" style="margin-left: -120px;">
+    
+                        <a class="mx-32 my-10"
+                            href="http://localhost/IlyasChaoui-Avito/UpdateTask.php?id=<?php echo $row['Id']; ?>"><svg
+                                xmlns="http://www.w3.org/2000/svg" height="30"
+                                viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                <style>
+                                    svg {
+                                        fill: #000000
+                                    }
+                                </style>
+                                <path
+                                    d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z" />
+                            </svg></a>
+                        <a class="my-10"
+                            href="http://localhost/IlyasChaoui-Avito/includes/delete.php?id=<?php echo $row['Id']; ?>"><svg
+                                xmlns="http://www.w3.org/2000/svg" height="30"
+                                viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                <style>
+                                    svg {
+                                        fill: #000000
+                                    }
+                                </style>
+                                <path
+                                    d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
+                            </svg></a>
+                    </div>
                 </div>
             </div>
             <script>
